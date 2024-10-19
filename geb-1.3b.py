dd23af47023f9ecb714d0656e81a1727e6ced2c0
Javen Zamojcin <javen.william@gmail.com>
2024-10-19 13:59:01 -0400

    impl init geb nb

--- geb-1.3b.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer, AutoModel
-import torch
-
-model = AutoModel.from_pretrained("GEB-AGI/geb-1.3b", trust_remote_code=True).bfloat16() #.cuda()
-tokenizer = AutoTokenizer.from_pretrained("GEB-AGI/geb-1.3b", trust_remote_code=True)
-
-query = "你好"
-response, history = model.chat(tokenizer, query, history=[])
-print(response)
